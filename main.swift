25ecda2f29e14439febf111f975d796231aba27a
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2025-09-25 23:12:48 +0530

    Using cocoa framework to show simple UI

--- main.swift
+++ main.swift
@@ -1,1 +1,6 @@
-print("Welcome")
+import Cocoa
+
+let alert = NSAlert()
+alert.messageText = "Welcome! This is my first macOS app"
+alert.informativeText = "Your first macOS app is running 🚀"
+alert.runModal()
